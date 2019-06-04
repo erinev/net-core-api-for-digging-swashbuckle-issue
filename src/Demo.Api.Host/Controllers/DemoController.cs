6220ac7dc6427b357862dce82545a97547952d36
Erikas Neverdauskas <e.neverdauskas@adform.com>
2019-06-04 21:01:21 +0300

    added apiDescriptionGroupCollection consoling for tracing

--- src/Demo.Api.Host/Controllers/DemoController.cs
+++ src/Demo.Api.Host/Controllers/DemoController.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace Demo.Api.Host.Controllers
 {
@@ -8,10 +10,29 @@ namespace Demo.Api.Host.Controllers
     [Produces("application/json")]
     public class DemoController : ControllerBase
     {
+        private readonly IApiDescriptionGroupCollectionProvider _apiDescriptionGroupCollection;
+
+        public DemoController(IApiDescriptionGroupCollectionProvider apiDescriptionGroupCollection)
+        {
+            _apiDescriptionGroupCollection = apiDescriptionGroupCollection;
+        }
+
         [HttpGet("resource")]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         public IActionResult GetResource()
         {
+            foreach (var apiDescriptionGroupItem in _apiDescriptionGroupCollection.ApiDescriptionGroups.Items)
+            {
+                foreach (var apiDescriptionItem in apiDescriptionGroupItem.Items)
+                {
+                    Console.WriteLine($"------------------------------------------------------------------");
+                    Console.WriteLine($"apiDescriptionItem.GroupName: {apiDescriptionItem.GroupName}");
+                    Console.WriteLine($"apiDescriptionItem.HttpMethod: {apiDescriptionItem.HttpMethod}");
+                    Console.WriteLine($"apiDescriptionItem.RelativePath: {apiDescriptionItem.RelativePath}");
+                    Console.WriteLine($"------------------------------------------------------------------");
+                }
+            }
+
             return Ok("resource");
         }
     }
